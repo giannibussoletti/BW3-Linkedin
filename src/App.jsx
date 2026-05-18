b0b00b8006bf2ded8089aa6f787e49a8fe4d7c5c
VincenzoManfredi99 <vincenzomanfredi2050@gmail.com>
2026-05-18 14:33:23 +0200

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import SidebarLeft from "./SidebarLeft";
 
 const App = function () {
   return (
     <>
       <h1>Progetto Linkedin</h1>
+      <SidebarLeft />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
