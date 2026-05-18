367a008d3144859cd88bf363c9f4959744f47719
giannibussoletti <bussolettigianni@gmail.com>
2026-05-18 10:27:39 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const App = function () {
+  return (
+    <>
+      <h1>Progetto Linkedin</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import { Provider } from "react-redux"
+
+createRoot(document.getElementById("root")).render(
+  <Provider>
+    <App />
+  </Provider>,
+)
