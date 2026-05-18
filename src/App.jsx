a4cba167449448e9edf774c2b531892fec7b0ac9
giannibussoletti <bussolettigianni@gmail.com>
2026-05-18 15:12:20 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 //
 import NavbarLinkedin from "./Components/NavbarLinkedin"
+import PostCard from "./Components/PostCard"
 /* import all the icons in Free Solid, Free Regular, and Brands styles */
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fas } from "@fortawesome/free-solid-svg-icons"
@@ -8,7 +9,6 @@ import { far } from "@fortawesome/free-regular-svg-icons"
 import { fab } from "@fortawesome/free-brands-svg-icons"
 
 library.add(fas, far, fab)
-import PostCard from "./Components/PostCard"
 
 const App = function () {
   return (
