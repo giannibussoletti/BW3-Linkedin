b14feb0ec28f0c1e32461bdd7bc8d3c9b3f5f3ab
giannibussoletti <bussolettigianni@gmail.com>
2026-05-22 17:14:58 +0200

    Update ImgUpdate.jsx

--- src/Components/ImgUpdate.jsx
+++ src/Components/ImgUpdate.jsx
@@ -6,14 +6,14 @@ const ImgUpdate = () => {
       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2YTBiMGZiMDA2YmJlOTAwMTVkZWU1OGEiLCJpYXQiOjE3NzkxMDk4MDgsImV4cCI6MTc4MDMxOTQwOH0.NcEsDXU_hRcTEeLEQZqtcnmcYnQvt2mj7zUv9tSJ22M`,
     },
     body: JSON.stringify({
-      name: "Gianni",
-      surname: "Bussoletti",
-      email: "g@b.it",
-      username: "GianniBussoletti",
+      name: "Mario",
+      surname: "Rossi",
+      email: "ma@R.it",
+      username: "MR",
       bio: "Faccio di necessità virtù e faccio di virtù una sicurezza",
       title: "Full Stack Web Developer",
       area: "Roma - Italia",
-      image: "https://cdn.pixabay.com/photo/2023/12/13/23/01/swirl-8447932_1280.jpg", // SERVER GENERATED, modificabile
+      image: "https://cdn.pixabay.com/photo/2026/05/18/18/44/18-44-14-412_1280.jpg", // SERVER GENERATED, modificabile
     }),
   })
     .then((res) =>
